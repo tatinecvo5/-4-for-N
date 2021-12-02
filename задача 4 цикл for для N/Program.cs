54b81546558b2f67e33faf75506a0c7682c375f0
User <User@SERGEY>
2021-12-02 21:19:01 +0300

    Добавьте файлы проекта.

---
+++ задача 4 цикл for для N/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задача_4_цикл_for_для_N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            for (; N > 0; --N)
+            {
+                //если раскрыть скобки и все посокращать получится NxN=N*N, может не так понял задачу
+                int NxN = 1 + (N - 2) * N + (2 * N - 1);
+                Console.WriteLine(NxN);
+            }
+            Console.ReadKey();
+        }
+    }
+}
